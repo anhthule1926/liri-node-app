6b70497739fbd05bfa290a70b575612d0ed73e1a
Thu Le <anhthu.le1926@gmail.com>
2019-06-18 01:01:50 -0500

    create switches for various commands

--- liri.js
+++ liri.js
@@ -1,9 +1,37 @@
-//read and set any environment variables with the dotenv package
+//read and set any environment 
 require("dotenv").config();
 
-//add the code required to import the keys.js file and store it in a variable
-var keys = require("./keys.js");
+// Import the request npm package.
+var request = require("request");
 
-//You should then be able to access your keys information like so
+// Import the API keys
+var keys = require("./keys");
+
+// Import the FS package for read/write.
+var fs = require("fs");
+
+// Import the node-spotify-api NPM package.
+var  Spotify = require ("node-spotify-api");
+
+// Initialize the spotify API client with keys
 var spotify = new Spotify(keys.spotify);
 
+
+//switches for various commands
+var command = process.argv[2];
+function switchStatement() {
+    switch (command) {
+        case "movie-this":
+            getMovie();
+            break;
+        case "concert-this":
+            concertThis();
+            break;
+        case "spotify-this-song":
+            getSong();
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+    }
+}
